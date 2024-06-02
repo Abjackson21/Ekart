cf4bb39824469359ccf97842317d6516d82ee7dd
Abjackson21 <abdullahsuhaib909@gmail.com>
2024-06-02 21:53:57 +0800

    first commit

---
+++ lib/screens/auth-ui/splash_screen.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+
+import 'dart:async';
+
+import 'package:ecomerce/screens/auth-ui/welcome_screen.dart';
+import 'package:ecomerce/utils/app_constant.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Get.offAll(() => WelcomeScreen());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppConstant.appSecondaryColor,
+      appBar: AppBar(
+        backgroundColor: AppConstant.appSecondaryColor,
+        elevation: 0,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                child: Lottie.asset('assets/images/splash_icon.json'),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 20.0),
+              width: Get.width,
+              alignment: Alignment.center,
+              child: Text(
+                AppConstant.appPoweredBy,
+                style: TextStyle(
+                    color: AppConstant.appTextColor,
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/auth-ui/welcome_screen.dart
@@ -0,0 +1,91 @@
+// ignore_for_file: file_names, prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables, prefer_const_constructors_in_immutables
+
+import 'package:ecomerce/screens/auth-ui/sign_in_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import '../../utils/app_constant.dart';
+import 'sign_up_screen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: AppConstant.appSecondaryColor,
+        title: Text(
+          "Welcome to my app",
+          style: TextStyle(color: AppConstant.appTextColor),
+        ),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              child: Lottie.asset('assets/images/splash_icon.json'),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20.0),
+              child: Text(
+                "Happy Shopping",
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: Get.height / 12,
+            ),
+            Material(
+              child: Container(
+                width: Get.width / 1.2,
+                height: Get.height / 12,
+                decoration: BoxDecoration(
+                  color: AppConstant.appMainColor,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: TextButton.icon(
+                  label: Text(
+                    "Sign in",
+                    style: TextStyle(color: AppConstant.appTextColor),
+                  ),
+                  onPressed: () {
+                    Get.to(() => SignInScreen());
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: Get.height / 50,
+            ),
+            Material(
+              child: Container(
+                width: Get.width / 1.2,
+                height: Get.height / 12,
+                decoration: BoxDecoration(
+                  color: AppConstant.appMainColor,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: TextButton.icon(
+                  label: Text(
+                    "Sign up",
+                    style: TextStyle(color: AppConstant.appTextColor),
+                  ),
+                  onPressed: () {
+                    Get.to(() => SignUpScreen());
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
